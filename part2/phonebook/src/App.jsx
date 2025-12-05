134b0d3936c0291891563961ec92507b58dc2330
Leonardo Andrade <105254884+leoandradej@users.noreply.github.com>
2025-12-04 21:35:49 -0300

    Phonebook step 4 completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,10 +21,17 @@ const App = () => {
       alert(`${newName} is already added to phonebook`);
       return;
     }
-    setPersons((prev) => [...prev, { name: newName, number: newNumber }]);
+    setPersons((prev) => [
+      ...prev,
+      { name: newName, number: newNumber, id: String(persons.length + 1) },
+    ]);
 
     setNewName("");
     setNewNumber("");
+  };
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
   };
 
   const handleNewName = (e) => {
@@ -31,10 +42,19 @@ const App = () => {
     setNewNumber(e.target.value);
   };
 
+  const filteredPersons = persons.filter((person) =>
+    person["name"].toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with:{" "}
+        <input type="text" value={search} onChange={handleSearch} />
+      </div>
       <form onSubmit={handleSubmit}>
+        <h2>Add a New Contact</h2>
         <div>
           name: <input type="text" value={newName} onChange={handleNewName} />
         </div>
@@ -47,7 +67,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {filteredPersons.map((person) => (
         <p key={person.name}>
           {person.name} {person.number}
         </p>
