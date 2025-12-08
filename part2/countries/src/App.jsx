6fbb011ffd2e0dcf71d7304666e7e4093e75941a
Leonardo Andrade <105254884+leoandradej@users.noreply.github.com>
2025-12-07 22:39:55 -0300

    Countries step 3 completed

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [selectedCountry, setSelectedCountry] = useState(null);
+  const [weather, setWeather] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
 
   const BASE_URL = "https://studies.cs.helsinki.fi/restcountries/api";
@@ -37,6 +38,22 @@ const App = () => {
   const countryToDisplay =
     selectedCountry ||
     (filteredCountries.length === 1 ? filteredCountries[0] : null);
+
+  useEffect(() => {
+    if (countryToDisplay && countryToDisplay.capital?.[0]) {
+      const API_KEY = import.meta.env.VITE_WEATHER_API_API_KEY;
+      const WEATHER_API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${countryToDisplay.capital[0]}&aqi=no`;
+
+      axios
+        .get(WEATHER_API_URL)
+        .then((response) => response.data)
+        .then((weatherData) => setWeather(weatherData))
+        .catch((error) => {
+          console.error("Error fetching weather:", error);
+          setWeather(null);
+        });
+    }
+  }, [countryToDisplay]);
 
   const renderCountries = () => {
     if (searchTerm === "") {
@@ -82,6 +99,14 @@ const App = () => {
           alt={`Flag of ${countryToDisplay.name.common}`}
           width="200"
         />
+        {weather && (
+          <>
+            <h2>Weather in {countryToDisplay.capital?.[0]}</h2>
+            <p>Temperature {weather.current.temp_c} Celsius</p>
+            <img src={weather.current.condition.icon} alt="Weather Icon" />
+            <p>Wind {weather.current.wind_kph} km/h</p>
+          </>
+        )}
       </div>
     );
   };
