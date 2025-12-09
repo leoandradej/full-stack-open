e708c10389d1d1c229d8be961a83b558cd8a3d3e
Leonardo Andrade <105254884+leoandradej@users.noreply.github.com>
2025-12-08 23:48:37 -0300

    Part3 Phonebook step 8 completed

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -25,8 +25,11 @@ persons = [
   },
 ];
 
-app.use(morgan("tiny"));
 app.use(express.json());
+morgan.token("body", (req) => JSON.stringify(req.body));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
