a5f900992f891c50d273e9be9ba2172a4c05dcdb
Leonardo Andrade <105254884+leoandradej@users.noreply.github.com>
2025-12-09 23:06:06 -0300

    Part3 Phonebook step 10 deploy setup

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -26,11 +26,12 @@ persons = [
   },
 ];
 
-app.use(express.json());
 morgan.token("body", (req) => JSON.stringify(req.body));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
+app.use(express.static("dist"));
+app.use(express.json());
 app.use(cors());
 
 app.get("/api/persons", (request, response) => {
